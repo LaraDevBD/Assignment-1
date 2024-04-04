5243ee71ae33917d1dfd18c90e743a849ce502ba
Luke Sarker <luke@barishaldiocese@gmail.com>
2024-04-05 03:33:38 +0600

    Checked if the second inputted value is 0 for division (/) Operation

--- calculator/res/js/calculator-script.js
+++ calculator/res/js/calculator-script.js
@@ -1,15 +1,34 @@
 function calculate(){
-    let firstNumber = document.getElementById('first-number').value;
-    let secondNumber = document.getElementById('second-number').value;
-    let selectedOperator = document.getElementById('operator').value;
+    let firstInput = document.getElementById('first-number');
+    let secondInput = document.getElementById('second-number');
+    let selectedOperator = document.getElementById('operator');
+    let resultDisplay = document.getElementById('result-display');
 
-    if(firstNumber==""){
-        alert("Please Input First Number");
+    // Checking if first Number is inputted
+    if(firstInput.value==""){
+        //alert("Please Input First Number");
+        resultDisplay.classList.replace('alert-info','alert-danger');
+        resultDisplay.textContent = "Please Input the First Number";
+        firstInput.classList.add('bg-danger');
     }
-    else if(secondNumber==""){
-        alert("Please Input Second Number");
+    // Checking if first Number is inputted
+    else if(secondInput.value==""){
+        //alert("Please Input Second Number");
+        resultDisplay.classList.replace('alert-info','alert-danger');
+        resultDisplay.textContent = "Please Input the Second Number";
+        firstInput.classList.replace('bg-danger','bg-success');
+        secondInput.classList.add('bg-danger');
     }
+    // Checking if second number is 0 in case of division (/) operation
+    else if((selectedOperator.value=="/")&&(secondInput.value==0)){
+        //alert(firstInput.value+" can't be devided with a 0");
+        resultDisplay.classList.replace('alert-info','alert-danger');
+        resultDisplay.textContent = firstInput.value+" can't be devided with a 0";
+        firstInput.classList.replace('bg-danger','bg-success');
+        secondInput.classList.add('bg-danger');
+    }
+    // If all checking done
     else{
-        alert("Okay");
+
     }
 }
