8aaacf1f5b908c2231b8085b1e6b4aa4c32d21c6
Luke Sarker <luke@barishaldiocese@gmail.com>
2024-04-05 02:25:27 +0600

    calculate function is created and triggered on clicking the Calculate Button

---
+++ calculator/res/js/calculator-script.js
@@ -0,0 +1,5 @@
+function calculate(){
+    let firstNumber = document.getElementById('first-number').value;
+    let secondNumber = document.getElementById('second-number').value;
+    let selectedOperator = document.getElementById('operator').value;
+}
