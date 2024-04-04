b24d315781de2dfd402b5edf26f0ed208c90ddb4
Luke Sarker <luke@barishaldiocese@gmail.com>
2024-04-05 02:33:09 +0600

    Checking if two numbers are inputted and alerted with instruction

--- calculator/res/js/calculator-script.js
+++ calculator/res/js/calculator-script.js
@@ -2,4 +2,14 @@ function calculate(){
     let firstNumber = document.getElementById('first-number').value;
     let secondNumber = document.getElementById('second-number').value;
     let selectedOperator = document.getElementById('operator').value;
+
+    if(firstNumber==""){
+        alert("Please Input First Number");
+    }
+    else if(secondNumber==""){
+        alert("Please Input Second Number");
+    }
+    else{
+        alert("Okay");
+    }
 }
