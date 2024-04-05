d1f2f326236f43fc7dd4327e75353030fb091e11
Luke Sarker <luke@barishaldiocese@gmail.com>
2024-04-06 01:45:20 +0600

    Two instance have been created

--- bank_app/bank-app.js
+++ bank_app/bank-app.js
@@ -4,9 +4,14 @@ class BankAccount{
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = balance;
+        console.log("===============================");
         console.log("Account Number: "+this.accountNumber);
         console.log("Owner Name: "+this.ownerName);
         console.log("Balance: $"+this.balance);
+        console.log("===============================");
     }
 
 }
+
+const bankAccount1 = new BankAccount(1234,"Habil Mondol",9999);
+const bankAccount2 = new BankAccount(4567,"Kabil Mondol",3099);
