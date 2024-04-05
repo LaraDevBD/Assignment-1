b546bf36f6817aa6bac143d673487be93d1383dd
Luke Sarker <luke@barishaldiocese@gmail.com>
2024-04-06 02:38:20 +0600

    Deopsit Functionality Demonstration Done

--- bank_app/bank-app.js
+++ bank_app/bank-app.js
@@ -22,13 +22,21 @@ class BankAccount{
         return this.balance;
     }
     displayAccountInfo(){
-        console.log("===============================");
+        console.log("=============================================");
         console.log("Account Number: "+this.accountNumber);
         console.log("Owner Name: "+this.ownerName);
         console.log("Balance: $"+this.balance);
-        console.log("===============================");
+        console.log("=============================================");
     }
 }
 
+console.log("First Account Created");
 const bankAccount1 = new BankAccount(1234,"Habil Mondol",9999);
+console.log("Second Account Created");
 const bankAccount2 = new BankAccount(4567,"Kabil Mondol",3099);
+
+// Deposit Functionality Demonstration
+bankAccount1.deposit(442);
+console.log("=============================================");
+console.log("After Deopsiting $442 in "+bankAccount1.accountNumber+" account numebr the new balance is: $"+bankAccount1.getBalance());
+console.log("=============================================");
