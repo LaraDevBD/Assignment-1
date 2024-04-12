51ca18819192f478af9343a5ad9037e247749f88
Luke Sarker <luke@barishaldiocese@gmail.com>
2024-04-12 21:47:17 +0600

    Handled if the inputted values are numbers using code

--- calculator/res/js/calculator-script.js
+++ calculator/res/js/calculator-script.js
@@ -4,26 +4,29 @@ function calculate(){
     let selectedOperator = document.getElementById('operator');
     let resultDisplay = document.getElementById('result-display');
 
-    // Checking if first Number is inputted
-    if(firstInput.value==""){
-        //alert("Please Input First Number");
-        resultDisplay.classList.replace('alert-info','alert-danger');
-        resultDisplay.textContent = "Please Input the First Number";
+    // Checking if first Number is inputted and the value is number
+    if((firstInput.value=="")||(isNaN(firstInput.value)==true)){
+        //alert("Please Check First Number");
+        resultDisplay.classList.remove('alert-info','alert-success');
+        resultDisplay.classList.add('alert-danger');
+        resultDisplay.textContent = "Please Check the First Number";
         firstInput.classList.add('bg-danger');
     }
-    // Checking if first Number is inputted
-    else if(secondInput.value==""){
-        //alert("Please Input Second Number");
-        resultDisplay.classList.replace('alert-info','alert-danger');
-        resultDisplay.textContent = "Please Input the Second Number";
+    // Checking if Second Number is inputted and the value is number
+    else if((secondInput.value=="")||(isNaN(secondInput.value)==true)){
+        //alert("Please Check Second Number");
+        resultDisplay.classList.remove('alert-info','alert-success');
+        resultDisplay.classList.add('alert-danger');
+        resultDisplay.textContent = "Please Check the Second Number";
         firstInput.classList.remove('bg-danger');
         secondInput.classList.add('bg-danger');
     }
     // Checking if second number is 0 in case of division (/) operation
     else if((selectedOperator.value=="/")&&(secondInput.value==0)){
-        //alert(firstInput.value+" can't be devided with a 0");
-        resultDisplay.classList.replace('alert-info','alert-danger');
-        resultDisplay.textContent = firstInput.value+" can't be devided with a 0";
+        //alert("A number can't be devided with a 0");
+        resultDisplay.classList.remove('alert-info','alert-success');
+        resultDisplay.classList.add('alert-danger');
+        resultDisplay.textContent = "A number can't be devided with a 0";
         firstInput.classList.remove('bg-danger');
         secondInput.classList.add('bg-danger');
     }
@@ -50,9 +53,8 @@ function calculate(){
                 theResult = parseFloat(firstInput.value)/parseFloat(secondInput.value);
             break;
         }
-        resultDisplay.classList.remove('alert-danger');
-        resultDisplay.classList.remove('alert-info');
-        resultDisplay.classList.add('text-white','bg-success');
-        resultDisplay.textContent = "Calculated Result= "+theResult.toFixed(2);
+        resultDisplay.classList.remove('alert-danger','alert-info');
+        resultDisplay.classList.add('alert-success');
+        resultDisplay.textContent = "Calculated Result= "+theResult.toFixed(2); 
     }
 }
